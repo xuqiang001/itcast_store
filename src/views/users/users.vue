dd5e5f697254c061680f0f3f629c47a6b91b2ab8
goddlts <54337467@qq.com>
2018-06-04 15:53:29 +0800

    '表格'

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -5,7 +5,7 @@
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>用户列表</el-breadcrumb-item>
     </el-breadcrumb>
-
+    <!-- 搜索框 -->
     <el-row class="searchContainer">
       <el-col :span="24"><div class="grid-content bg-purple-dark">
         <el-input
@@ -18,12 +18,79 @@
         <el-button type="success" plain>添加用户</el-button>
       </div></el-col>
     </el-row>
+    <!-- 表格 -->
+    <el-table
+      stripe
+      border
+      :data="tableData"
+      style="width: 100%">
+      <el-table-column
+        type="index"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="180">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="primary"
+            icon="el-icon-edit"
+            plain>
+          </el-button>
+          <el-button
+            size="mini"
+            type="danger"
+            icon="el-icon-delete"
+            plain>
+          </el-button>
+          <el-button
+            type="success"
+            icon="el-icon-check"
+            size="mini"
+            plain>
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }]
+    }
+  }
 };
 </script>
 
@@ -34,6 +101,7 @@ export default {
   }
   .searchContainer {
     margin-top: 15px;
+    margin-bottom: 15px;
   }
   .searchInput {
     width: 300px;
