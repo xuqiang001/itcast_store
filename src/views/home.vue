50bfe7ea6c636a855c8414feb9024bd8c4b79cc4
goddlts <54337467@qq.com>
2018-06-04 11:54:04 +0800

    'home界面完成'

--- src/views/home.vue
+++ src/views/home.vue
@@ -14,8 +14,83 @@
       </el-row>
     </el-header>
     <el-container class="container">
-      <el-aside class="aside" width="200px">Aside</el-aside>
-      <el-main class="main">Main</el-main>
+      <el-aside class="aside" width="200px">
+        <el-menu
+          :unique-opened="true"
+          :router="true"
+          default-active="1-1"
+          class="menu">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              用户列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              角色列表
+            </el-menu-item>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              权限列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>商品管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              商品列表
+            </el-menu-item>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              分类参数
+            </el-menu-item>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              商品分类
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="4">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>订单管理</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              订单列表
+            </el-menu-item>
+          </el-submenu>
+          <el-submenu index="5">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>数据统计</span>
+            </template>
+            <el-menu-item index="users">
+              <i class="el-icon-menu"></i>
+              数据报表
+            </el-menu-item>
+          </el-submenu>
+          
+ 
+ 
+ 
+        </el-menu>
+      </el-aside>
+      <el-main class="main">
+        <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
@@ -67,9 +142,14 @@ export default {
   background-color: #d3dce6;
 }
 
+.aside .menu {
+  height: 100%;
+}
+
 .main {
   height: 100%;
   background-color: #e9eef3;
 }
+
 </style>
 
