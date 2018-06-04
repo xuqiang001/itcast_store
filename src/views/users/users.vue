ad893a0c112a1af28da2e50569d7e8477cafa1f1
goddlts <54337467@qq.com>
2018-06-04 16:43:36 +0800

    '日期格式化'

--- src/views/users/users.vue
+++ src/views/users/users.vue
@@ -43,8 +43,10 @@
         label="电话">
       </el-table-column>
       <el-table-column
-        prop="create_time"
         label="创建时间">
+        <template slot-scope="scope">
+          {{ scope.row.create_time | fmtDate('YYYY-MM-DD') }}
+        </template>
       </el-table-column>
       <el-table-column
         label="用户状态">
