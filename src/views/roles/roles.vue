4aeef0fa5e94e9b9078e048d89c0ee5385e6dcbb
goddlts <54337467@qq.com>
2018-06-07 10:16:09 +0800

    一级和二级权限

--- src/views/roles/roles.vue
+++ src/views/roles/roles.vue
@@ -14,20 +14,58 @@
 
     <el-table
       :data="tableData"
+      v-loading="loading"
       stripe
       border
       style="width: 100%">
+      <el-table-column type="expand">
+        <template slot-scope="scope">
+          <!-- 展开之后，显示一级权限 -->
+          <el-row
+            class="level1"
+            v-for="item in scope.row.children"
+            :key="item.id">
+            <el-col :span="4">
+              <!-- 显示一级权限的名称 -->
+              <el-tag
+                closable>
+                {{ item.authName }}
+              </el-tag>
+            </el-col>
+            <el-col :span="20">
+              <!-- 显示二、三级权限 -->
+              <el-row
+                class="level2"
+                v-for="item1 in item.children"
+                :key="item1.id">
+                <!-- 显示二级权限 -->
+                <el-col :span="4">
+                  <el-tag
+                    type="success"
+                    closable>
+                    {{ item1.authName }}
+                  </el-tag>
+                </el-col>
+                <!-- 显示三级权限 -->
+                <el-col :span="20">
+
+                </el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </template>
+      </el-table-column>
       <el-table-column
         type="index"
         width="40">
       </el-table-column>
       <el-table-column
-        prop="authName"
+        prop="roleName"
         label="角色名称"
         width="180">
       </el-table-column>
       <el-table-column
-        prop="path"
+        prop="roleDesc"
         label="角色描述"
         width="180">
       </el-table-column>
@@ -62,8 +100,29 @@
 export default {
   data() {
     return {
-      tableData: []
+      tableData: [],
+      loading: true
     };
+  },
+  created() {
+    this.loadData();
+  },
+  methods: {
+    async loadData() {
+      // 响应对象 res = { data, status }
+      // 服务器返回的响应数据在 res.data
+      // res.data = { data: [], meta: {} }
+      const { data: resData } = await this.$http.get('roles');
+      const { data, meta } = resData;
+
+      this.loading = false;
+      if (meta.status === 200) {
+        // 成功
+        this.tableData = data;
+      } else {
+        this.$message.error(meta.msg);
+      }
+    }
   }
 };
 </script>
@@ -72,5 +131,12 @@ export default {
 .row {
   margin-top: 15px;
   margin-bottom: 15px;
+}
+.level1 {
+  margin-bottom: 15px;
+}
+
+.level2 {
+  margin-bottom: 5px;
 }
 </style>
