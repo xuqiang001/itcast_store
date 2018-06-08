af2ecaa260568c8eb0052d8d5fea0341f078dbe5
goddlts <54337467@qq.com>
2018-06-08 15:55:41 +0800

    商品分类下拉框

--- src/views/goods/goodsadd.vue
+++ src/views/goods/goodsadd.vue
@@ -22,13 +22,42 @@
       <el-step title="商品内容"></el-step>
     </el-steps>
 
-    <el-tabs v-model="active" class="tabs" tab-position="left">
-      <el-tab-pane name="0" label="基本信息">基本信息</el-tab-pane>
-      <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
-      <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
-      <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
-      <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
-    </el-tabs>
+    <el-form
+      style="height: 500px; overflow: auto"
+      label-position="top"
+      :model="form"
+      label-width="100px">
+      <el-tabs v-model="active" class="tabs" tab-position="left">
+        <el-tab-pane name="0" label="基本信息">
+          <el-form-item label="商品名称">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品数量">
+            <el-input></el-input>
+          </el-form-item>
+          <el-form-item label="商品分类">
+            <el-cascader
+              clearable
+              :options="options"
+              :props="defaultProps"
+              v-model="selectedOptions"
+              expand-trigger="hover"
+              @change="handleChange">
+            </el-cascader>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>
+    </el-form>
   </el-card>
 </template>
 
@@ -36,8 +65,39 @@
 export default {
   data() {
     return {
-      active: '0'
+      active: '0',
+      form: {
+      },
+      // 层级下拉框的数据源
+      options: [],
+      // 层级下拉框的数据源配置
+      defaultProps: {
+        value: 'cat_id',
+        label: 'cat_name',
+        children: 'children'
+      },
+      // 绑定到层级下拉框上的数据
+      selectedOptions: []
     };
+  },
+  created() {
+    this.loadOptions();
+  },
+  methods: {
+    // 层级下拉框选中内容发生变化的时候执行
+    handleChange() {
+      // 判断当前是否选中的是三级分类
+      // 如果不是三级分类，此时提示，并且清空
+      if (this.selectedOptions.length !== 3) {
+        this.$message.warning('请选择三级分类,商品只能添加到三级分类');
+        this.selectedOptions.length = 0;
+      }
+    },
+    // 加载层级下拉框的数据
+    async loadOptions() {
+      const { data: resData } = await this.$http.get('categories?type=3');
+      this.options = resData.data;
+    }
   }
 };
 </script>
