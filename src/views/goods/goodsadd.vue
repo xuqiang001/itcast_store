0bfdb89e234838a86885f0e30f988b99fe2767a1
goddlts <54337467@qq.com>
2018-06-08 15:19:20 +0800

    tabs和步骤条

--- src/views/goods/goodsadd.vue
+++ src/views/goods/goodsadd.vue
@@ -13,20 +13,32 @@
 
     <el-steps
       align-center
-      :active="2"
+      :active="active - 0"
       finish-status="success">
-      <el-step class="steps" title="基本信息"></el-step>
+      <el-step title="基本信息"></el-step>
       <el-step title="商品参数"></el-step>
       <el-step title="商品属性"></el-step>
-      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
       <el-step title="商品内容"></el-step>
     </el-steps>
+
+    <el-tabs v-model="active" class="tabs" tab-position="left">
+      <el-tab-pane name="0" label="基本信息">基本信息</el-tab-pane>
+      <el-tab-pane name="1" label="商品参数">商品参数</el-tab-pane>
+      <el-tab-pane name="2" label="商品属性">商品属性</el-tab-pane>
+      <el-tab-pane name="3" label="商品图片">商品图片</el-tab-pane>
+      <el-tab-pane name="4" label="商品内容">商品内容</el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      active: '0'
+    };
+  }
 };
 </script>
 
@@ -34,5 +46,8 @@ export default {
   .alert {
     margin-top: 15px;
     margin-bottom: 15px;
+  }
+  .tabs {
+    margin-top: 15px;
   }
 </style>
