eb345ca3acae17a1be2faa1d17a598775480992f
goddlts <54337467@qq.com>
2018-06-08 15:05:56 +0800

    步骤条

--- src/views/goods/goodsadd.vue
+++ src/views/goods/goodsadd.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>123</div>
+  <el-card class="box-card">
+    <my-breadcrumb level1="商品管理" level2="商品列表"></my-breadcrumb>
+
+    <el-alert
+      class="alert"
+      title="添加商品信息"
+      type="info"
+      center
+      :closable="false"
+      show-icon>
+    </el-alert>
+
+    <el-steps
+      align-center
+      :active="2"
+      finish-status="success">
+      <el-step class="steps" title="基本信息"></el-step>
+      <el-step title="商品参数"></el-step>
+      <el-step title="商品属性"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
+    </el-steps>
+  </el-card>
 </template>
 
 <script>
@@ -8,6 +30,9 @@ export default {
 };
 </script>
 
-<style>
-
+<style scoped>
+  .alert {
+    margin-top: 15px;
+    margin-bottom: 15px;
+  }
 </style>
