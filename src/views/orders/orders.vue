f6b2c8a28e636dc1fd042f68364c75669f5552a2
goddlts <54337467@qq.com>
2018-06-10 16:57:38 +0800

    省市选择

--- src/views/orders/orders.vue
+++ src/views/orders/orders.vue
@@ -40,6 +40,7 @@
         label="操作">
         <template slot-scope="scope">
           <el-button
+            @click="dialogFormVisible = true"
             size="mini"
             type="primary"
             icon="el-icon-edit"
@@ -48,17 +49,44 @@
         </template>
       </el-table-column>
     </el-table>
+
+    <el-dialog title="修改订单地址" :visible.sync="dialogFormVisible">
+      <el-form label-width="100px">
+        <el-form-item label="省市区/县">
+           <el-cascader
+            style="width: 500px"
+            expand-trigger="hover"
+            :options="options"
+            v-model="selectedOptions">
+          </el-cascader>
+        </el-form-item>
+        <el-form-item label="详细地址">
+          <el-input auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
   </el-card>
 </template>
 
 <script>
+// 导入数据模块
+import data from './city_data2017_element';
+
 export default {
   data() {
     return {
-      tableData: []
+      tableData: [],
+      dialogFormVisible: false,
+      options: [],
+      selectedOptions: []
     };
   },
   created() {
+    this.options = data;
     this.loadData();
   },
   methods: {
