5bc45f943f6938c2b87518360d9bcd6cbb10cfc0
goddlts <54337467@qq.com>
2018-06-10 11:18:53 +0800

    分类参数-级联下拉框

--- src/views/goods/params.vue
+++ src/views/goods/params.vue
@@ -16,7 +16,9 @@
         <el-cascader
           expand-trigger="hover"
           :options="options"
-          :props="{ label: '', value: '' }"
+          :show-all-levels="false"
+          @change="handleChange"
+          :props="{ label: 'cat_name', value: 'cat_id' }"
           v-model="selectedOptions">
         </el-cascader>
       </el-col>
@@ -25,7 +27,7 @@
     <!-- v-model 绑定的是 激活的tab-pane的name属性的值 -->
     <el-tabs v-model="activeName">
       <el-tab-pane label="动态参数" name="many">
-        <el-button type="primary">添加动态参数</el-button>
+        <el-button :disabled="isDisabled" type="primary">添加动态参数</el-button>
         <el-table
           :data="dynamicParams"
           style="width: 100%">
@@ -63,7 +65,7 @@
         </el-table>
       </el-tab-pane>
       <el-tab-pane label="静态参数" name="only">
-        <el-button type="primary">添加静态参数</el-button>
+        <el-button :disabled="isDisabled" type="primary">添加静态参数</el-button>
         <el-table
           :data="staticParams"
           style="width: 100%">
@@ -112,8 +114,33 @@ export default {
       selectedOptions: [],
       activeName: 'many',
       dynamicParams: [],
-      staticParams: []
+      staticParams: [],
+      isDisabled: true
     };
+  },
+  created() {
+    this.loadOptions();
+  },
+  methods: {
+    // 层级下拉框 发生改变
+    handleChange() {
+      // 判断 当前是否选择的3 级分类
+      // 如果选择的3级分类  此时按钮可用
+      if (this.selectedOptions.length === 3) {
+        // 可用
+        this.isDisabled = false;
+      } else {
+        // 不可用
+        this.isDisabled = true;
+      }
+    },
+    // 加载层级下拉框的数据
+    async loadOptions() {
+      const { data: { data, meta: { status } } } = await this.$http.get('categories?type=3');
+      if (status === 200) {
+        this.options = data;
+      }
+    }
   }
 };
 </script>
